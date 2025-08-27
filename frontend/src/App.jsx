42419781a9311d7a096a58b6de402aa005441a30
manoharsingh77 <manoharsvnit07@gmail.com>
2025-08-27 23:54:51 +0530

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,51 +2,61 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/Signup";
+import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-  const { authUser } = useAuthContext();
+	const { authUser } = useAuthContext();
 
-  return (
-    <div className="min-h-screen bg-[url('/bg.png')] bg-cover bg-center">
-      <Routes>
-        {/* Home */}
-        <Route path='/' element={authUser ? <Home /> : <Navigate to='/login' />} />
+	return (
+		<div className="min-h-screen bg-[url('/bg.png')] bg-cover bg-center flex items-center justify-center">
+			<Routes>
+				{/* Home (chat screen) */}
+				<Route
+					path="/"
+					element={
+						authUser ? (
+							<div className="w-full h-full flex items-center justify-center p-2">
+								<Home />
+							</div>
+						) : (
+							<Navigate to="/login" />
+						)
+					}
+				/>
 
-        {/* Login */}
-        <Route
-          path='/login'
-          element={
-            authUser ? (
-              <Navigate to='/' />
-            ) : (
-              <div className="p-4 min-h-screen flex items-center justify-center">
-                <Login />
-              </div>
-            )
-          }
-        />
+				{/* Login */}
+				<Route
+					path="/login"
+					element={
+						authUser ? (
+							<Navigate to="/" />
+						) : (
+							<div className="p-4 min-h-screen flex items-center justify-center">
+								<Login />
+							</div>
+						)
+					}
+				/>
 
-        {/* Signup */}
-        <Route
-          path='/signup'
-          element={
-            authUser ? (
-              <Navigate to='/' />
-            ) : (
-              <div className="p-4 min-h-screen flex items-center justify-center">
-                <SignUp />
-              </div>
-            )
-          }
-        />
-      </Routes>
-
-      <Toaster />
-    </div>
-  );
+				{/* Signup */}
+				<Route
+					path="/signup"
+					element={
+						authUser ? (
+							<Navigate to="/" />
+						) : (
+							<div className="p-4 min-h-screen flex items-center justify-center">
+								<SignUp />
+							</div>
+						)
+					}
+				/>
+			</Routes>
+			<Toaster />
+		</div>
+	);
 }
 
 export default App;
